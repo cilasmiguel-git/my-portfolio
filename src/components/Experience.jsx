05b3a0feac4bb48980bac5faf6418d469b25949c
unknown <email>
2023-02-03 23:06:45 -0300

    acrecentei notranslate em experiencias

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -80,7 +80,7 @@ const Experience = () => {
           <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">Experience</p>
           <p className="py-6">these are the technologies i've studied wich</p>
         </div>
-        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0">
+        <div className="w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0 notranslate">
           {techs.map(({id,src,title,style})=>(
           <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded-lg ${style}`}>
             <img src={src} alt="" className="w-20 mx-auto" />
