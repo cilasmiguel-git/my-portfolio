740dd1e72c209b4a4570a42efbf689e8bf20724b
Miguel <cilasmiguelcolacobp@gmail.com>
2025-08-25 14:29:55 +0000

    chore: define distDir=build para alinhar com Vercel

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+// next.config.mjs
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // faz o Next gerar a pasta de build como "build" (em vez de ".next")
+    distDir: 'build',
+    // (opcional) se estiver usando app router e imagens remotas, configure aqui domains, etc.
+  };
+  
+  export default nextConfig;
+  
